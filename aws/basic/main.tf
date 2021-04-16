1eeccd54eac772ecd15ee5297b6365bb0e7d495b
Cole Ratner <kolamiti92@gmail.com>
2021-04-15 22:50:18 -0400

    test

--- aws/basic/main.tf
+++ aws/basic/main.tf
@@ -18,3 +18,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
