1caba29db79e66de2886eefea144199fb1abab71
Cole Ratner <kolamiti92@gmail.com>
2021-04-21 14:45:11 -0400

    testing new ec2 instance creation

--- aws/basic/main.tf
+++ aws/basic/main.tf
@@ -19,7 +19,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
 
   tags = {
