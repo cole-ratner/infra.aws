86faea1ed7e1b1698f185064a539b8dd9dcba462
Cole Ratner <kolamiti92@gmail.com>
2021-04-23 09:21:46 -0400

    testing module usage

--- main.tf
+++ main.tf
@@ -15,15 +15,11 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = lookup(var.region)
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0742b4e673072066f"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "testInstance1"
-  }
+module "app_server" {
+  source = "github.com/cole-ratner/terraform_modules/app_server"
+  instance_name = "testInstance"
+  ami_id = "ami-0742b4e673072066f"
 }
-
