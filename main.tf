d0196bc9bc99395d84cd76231be369b4c2717def
Cole Ratner <kolamiti92@gmail.com>
2021-04-23 10:18:58 -0400

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+# -------------------------------------------------------------------------------------------
+# Declaring the provider & a remote backend hosted by terraform cloud to manage state 
+# and to perform the terraform operations remotely
+# -------------------------------------------------------------------------------------------
 terraform {
   backend "remote" {
     organization = "Gravityloop"
@@ -12,14 +16,17 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   profile = "default"
   region  = var.region
 }
 
+# -------------------------------------------------------------------------------------------
+# Defining infra resources to provision by the previously declared provider
+# -------------------------------------------------------------------------------------------
 module "app_server" {
   source        = format("%s/app_server", lookup(var.custom_module_base_url))
   instance_name = "testInstance"
   ami_id        = "ami-0742b4e673072066f"
 }
+
