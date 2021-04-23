2db386ddf7b3b47a458ebd89ffa51d941b039389
Cole Ratner <kolamiti92@gmail.com>
2021-04-23 11:09:49 -0400

    module source will not take a var - must hardcode

--- main.tf
+++ main.tf
@@ -21,15 +21,11 @@ provider "aws" {
   region  = var.region
 }
 
-locals {
-  custom_module_base_url = "github.com/cole-ratner/terraform_modules"
-}
-
 # -------------------------------------------------------------------------------------------
 # Defining infra resources to provision by the previously declared provider
 # -------------------------------------------------------------------------------------------
 module "app_server" {
-  source        = "${local.custom_module_base_url}/app_server"
+  source        = "github.com/cole-ratner/terraform_modules/app_server"
   instance_name = "testInstance"
   ami_id        = "ami-0742b4e673072066f"
 }
